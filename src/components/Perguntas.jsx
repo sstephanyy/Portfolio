feaefcb448fc3b24274441ca2633f327b484fd01
Stephany <stephany18@mail.com>
2023-11-23 09:18:13 -0300

    changed the font size

--- src/components/Perguntas.jsx
+++ src/components/Perguntas.jsx
@@ -94,10 +94,11 @@ const Perguntas = () => {
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
-          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
-            Perguntas Frequentes
-          </p>
-          <p className="py-6">Tiramos todas as suas dúvidas</p>
+        <p className={window.innerWidth < 600 ? "text-3xl font-bold border-b-4 border-gray-500 p-1 inline" : "text-4xl"} font-bold border-b-4 border-gray-500 p-2 inline>
+          Perguntas Frequentes
+        </p>
+        
+        <p className="py-6">Tiramos todas as suas dúvidas</p>
         </div>
 
         <div className="w-full grid grid-cols-1 gap-8 text-center py-8 px-12 sm:px-0">
